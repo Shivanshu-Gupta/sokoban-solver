630bef22b539d69e61f24b10dafde87fc4eb0b35
Shivanshu Gupta <shivanshugupta1995@gmail.com>
2020-12-17 20:53:24 +0530

    minor modifications to run-tests.py

--- src/run-tests.py
+++ src/run-tests.py
@@ -1,8 +1,12 @@
 import subprocess
 import itertools
+import sys
+
+program = './main' if len(sys.argv) == 1 else sys.argv[1]
 
 for n in [6, 7, 8, 9, 10]:
     for b, i in list(itertools.product([1, 2, 3, 4], [1, 2])):
         test_file = '../samples/test_cases/n{0}/n{0}b{1}_{2}'.format(n, b, i)
-        process = subprocess.run(['./main', test_file])
+        print("solving {}".format(test_file))
+        process = subprocess.run([program, test_file])
 
